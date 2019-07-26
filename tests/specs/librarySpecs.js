4b7ecbdb8346e1a9b1541f3769b62a04b738960a
Ubuntu <ubuntu@ip-172-31-23-241.ap-southeast-1.compute.internal>
2019-07-26 08:33:13 +0000

    Test async function success

--- tests/specs/librarySpecs.js
+++ tests/specs/librarySpecs.js
@@ -1,7 +1,8 @@
 describe("Library Function Test", describe("Fetch users", function(){
-    it("should be able to fetch users", function(){
+    it("should be able to fetch users", function(done){
         getUsersAysnc(function(data) {
-            expect(data.length).toBeGreaterThan(0)
+            expect(data.length).toBeGreaterThan(0);
+            done();
         })
     })
 }));
